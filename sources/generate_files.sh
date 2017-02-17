159eecb0cb136071d40a2720333d8d8c6f4e5746
Nick Boughton <nicholasboughton@gmail.com>
2017-02-17 15:23:08 +0000

    removed peter rabbit

--- sources/generate_files.sh
+++ sources/generate_files.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd $(readlink -f $(dirname $0))
 cat lovecraft.txt trump.txt > trumpcraft.txt
-cat peter_rabbit.txt lovecraft.txt > peter_rabbitcraft.txt
-cat peter_rabbit.txt lovecraft.txt trump.txt > all.txt
